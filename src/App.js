55d350b20ad77e2de762354d05fe4ad98490fc3c
Tiffany Lam <tl_4208@msn.com>
2020-12-20 03:32:41 -0800

    Added github star button

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ class App extends Component {
             <br />
             Inspired by <a className='App-link' href='https://nicothin.pro/lessColourFunctionCalculator/' target='_blank' rel="noreferrer" title='LESS Colour Function Calculator'>LESS Colour Function Calculator</a> and my teammates' struggles.
           </small></p>
+          <iframe src="https://ghbtns.com/github-btn.html?user=lam-tiffany&repo=ff-brand-colors-generator&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
         </footer>
       </div>
     );
